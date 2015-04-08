80343944dc1ab1f20b027a57837e7762b4376aff
Thomas Thune Hansen <tth@bellcom.dk>
2015-04-08 10:31:39 +0200

    Added text field and city field

--- block-bc-subsite-subsite-contact.tpl.php
+++ block-bc-subsite-subsite-contact.tpl.php
@@ -10,6 +10,7 @@
 ?>
 <h2><?php print $name; ?></h2>
 <?php print $address; ?><br />
-<?php print $zipcode; ?><br />
+<?php print $zipcode; ?> <?php print $city; ?><br />
 <?php print $phone; ?><br />
 <?php print $mail; ?><br />
+<?php print $text; ?><br />
